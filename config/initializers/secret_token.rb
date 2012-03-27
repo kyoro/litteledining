a1a6028b941d72638472fc9348a3014d583025d7
Kyosuke INOUE <kyoro@hakamastyle.net>
2012-03-27 03:39:35 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LitteleDining::Application.config.secret_token = 'f5f90f7b65a4b43eff5e576f91bd71d98230a5932d2126e57fa325348f6d18d8959df25f8f5937fdef4ea91b7153f8ede8e4b79c2e9a6b772f3299dca3ac4223'
